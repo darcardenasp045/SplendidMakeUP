f678fec5e90f83fb7c73a21cf74462fbc0f32072
Daniel Cardenas <97079553+darcardenasp045@users.noreply.github.com>
2023-11-22 11:19:33 -0500

    Mejoras en indentacion

--- script/index.js
+++ script/index.js
@@ -1,5 +1,3 @@
-// Login PopUp JS
-
 var loginn = document.getElementById("loginn");
 var newAccount = document.getElementById("newAccount");
 var log = document.getElementById("log");
@@ -40,34 +38,6 @@ function hideSection(section) {
 }
 
 
-// Login Data JS
 
-const botonCrearCuenta = document.getElementById('sub');
-
-botonCrearCuenta.addEventListener("click", ()=>{
-  const nombreUsuario = document.querySelector("nombre").value;
-  const apellidoUsuario = document.querySelector("apellido").value;
-  const emailUsuario = document.querySelector("email").value;
-  const passwordUsuario = document.querySelector("pass").value;
-  if (nombreUsuario === "" ||apellidoUsuario === "" || emailUsuario === "" || passwordUsuario === "") {
-    alert('Todos los campos son obligatorios');
-  } else {
-    // Creamos el usuario con su información
-    let userData = {
-        name: nombreUsuario,
-        apellido:apellidoUsuario,
-        email: emailUsuario,
-        password: passwordUsuario
-
-        };
-        console.log(userData);
-        // Guardamos el usuario en localStorage
-        localStorage.setItem('user', JSON.stringify(userData));
-        
-        // Redireccionamos a la página principal
-        window.location.href="index.html"
-        }
-        
-})
 
 
